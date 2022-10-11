9a3d963cafa8b0127ba3f7e3996d0cd54e18d9e9
Kaustav Kakoty <90276199+kaustav4real@users.noreply.github.com>
2022-10-11 13:31:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayMenu = () => {
+  document.getElementById("ham-menu").classList.toggle("display-ham-menu");
+  document.getElementById("brand-name").classList.toggle("animate-brand");
+};
